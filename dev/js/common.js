8c3f3073b25b6dd6091f46581127957a09f32c1c
Pavel Borisenko <pavelborisenko@list.ru>
2019-07-05 18:53:22 +0700

    commit

--- dev/js/common.js
+++ dev/js/common.js
@@ -8,5 +8,26 @@ $(document).ready(function () {
 			el: '.swiper-pagination',
 			type: 'bullets',
 		},
-});
+	});
+
+	var reviewsSlider = new Swiper('.reviews-slider', {
+		speed: 400,
+		slidesPerView: 3,
+		spaceBetween: 50,
+		slidesPerGroup: 3,
+	});
+
+	$(window).scroll(function () {
+		var movement = -parseInt($(this).scrollTop() / 2.5);
+		$('.sections-wrapper').css({
+		backgroundPosition: 'center ' + (movement+800) + 'px'
+		});
+		});
+		
+	$('.section-park-video').scroll(function () {
+		var movement2 = -parseInt($(this).scrollTop() / 10);
+		$('.park-section-bg').css('transform', 'scale(' + movement2 +')');
+		console.log(movement2)
+		});
+
 });
